dfbb967716906b2ed20c06e13c4265d83f8af1cd
Saife <legendsaife321@gmail.com>
2023-05-16 23:47:06 +0500

    I added a geolocation library from flutter packages in pubspec.yaml file

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  get latitude => null;
+  get timestamp => null;
+  get accuracy => null;
+  get heading => null;
+  get speed => null;
+  get speedAccuracy => null;
+
+  void getLocation() async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(Position(longitude: 10,
+        latitude: latitude,
+        timestamp: timestamp,
+        accuracy: accuracy,
+        altitude: latitude,
+        heading: heading,
+        speed: speed,
+        speedAccuracy: speedAccuracy));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
           onPressed: () {
             //Get the current location
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );
   }
 }
 
-RaisedButton({Null Function() onPressed, Text child}) {
-
-}
